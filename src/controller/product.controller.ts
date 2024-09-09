be65eaee1d905508d00441e21d998c9d9e4c738c
DonySCode <donycastro80@gmail.com>
2024-09-09 00:56:58 -0400

    feat: Added authenticate method

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,6 +1,27 @@
-import {Request, Response} from 'express';
+import {Request, response, Response} from 'express';
 import {Product} from '../model/product';
+import * as jwt from "jsonwebtoken";
 import db from '../config/db';
+
+const authenticate = (email: string, token: string) => {
+    return new Promise( async (resolve, reject) => {
+        try{
+            let realToken: string;
+            let userId: string;
+            db.executeQuery(`SELECT userId FROM users WHERE email = "${email}"`)
+                .then(response => {
+                    console.log(response[0].userId);
+                    userId = response[0].userId;
+                    realToken = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+                    console.log(realToken)
+                    console.log(token)
+                    resolve(realToken === token);
+                }).catch(error => reject(error))
+        }catch(e){
+            reject(e);
+        }
+    })
+}
 
 const getProducts = async (req: Request, res: Response) => {
     db.executeQuery('SELECT * FROM products')
@@ -41,19 +62,27 @@ const getProduct = async (req: Request, res: Response) => {
 }
 
 const addProduct = async (req: Request, res: Response) => {
-    db.executeQuery(`INSERT INTO products (name, price) VALUES ("${req.body.name}", ${req.body.price})`)
-        .then((response: Product) => {
-            res.status(200).send({
-                message: 'OK',
-                result: response
+    const authenticated = await authenticate(req.body.email, req.body.token);
+    if (authenticated) {
+        db.executeQuery(`INSERT INTO products (name, price) VALUES ("${req.body.name}", ${req.body.price})`)
+            .then((response: Product) => {
+                res.status(200).send({
+                    message: 'OK',
+                    result: response
+                });
+            }).catch(error => {
+            console.log(error)
+            res.status(500).send({
+                message: `INTERNAL SERVER ERROR`,
+                result: null
             });
-        }).catch(error => {
-        console.log(error)
-        res.status(500).send({
-            message: `INTERNAL SERVER ERROR`,
+        })
+    } else {
+        res.status(401).send({
+            message: `Unauthorized user`,
             result: null
         });
-    })
+    }
 }
 
 const editProduct = async (req: Request, res: Response) => {
